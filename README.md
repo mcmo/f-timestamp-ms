e3bb6271de2037adb1fe151e296a00f2b4809756
mcmo <cmcmordie@charter.net>
2016-03-05 00:49:39 +0000

    added date handler

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ## Timestamp Microservice
 
+Website: <a href="https://f-timestamp-ms.herokuapp.com/" target="_blank">f-timestamp-ms.herokuapp.com</a>
+
 #### This is a <a href="http://www.freecodecamp.com/mcmo" target="_blank">FreeCodeCamp.com</a> project which must use full stack JavaScript to accomplish the following objectives.
 
 1.  I can pass a string as a parameter, and it will check to see whether that string contains either a unix timestamp or a natural language date (example: January 1, 2016).
--- server.js
+++ server.js
@@ -1,17 +1,31 @@
+'use strict'
 const Hapi = require('hapi')
 
 const server = new Hapi.Server()
 
 server.connection({
-    port: process.env.PORT || 8080
+  port: process.env.PORT || 8080
 })
 
+function handler(request, reply) {
+  const months = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"]
+  
+  const d = request.params.date
+  let date = new Date(d)
+  date = date == 'Invalid Date' ? new Date(Number(d)) : date
+
+  let dateObj = {
+    unix: date.getTime(),
+    natural: date != 'Invalid Date' ? months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear() : null
+  }
+  reply(dateObj)
+}
+
 server.route({
-    method: 'GET',
-    path: '/',
-    handler: (request, reply) => {
-        reply('hello hapi')
-    }
+  method: 'GET',
+  path: '/{date}',
+  handler: handler
 })
 
 server.start(() => console.log('Started'))
